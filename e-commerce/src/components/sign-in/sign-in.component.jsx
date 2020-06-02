ca7340ff2dbd4f72ea1ac4eab972acc36c2964b7
Thai Duc Huy <huy.thai@ymail.com>
2020-06-02 17:26:13 +0700

    finish sign in and sign up

--- e-commerce/src/components/sign-in/sign-in.component.jsx
+++ e-commerce/src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.util';
+import {auth, signInWithGoogle} from '../../firebase/firebase.util';
 
 class SignInComponent extends React.Component {
     constructor(props) {
@@ -14,12 +14,20 @@ class SignInComponent extends React.Component {
         };
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault();
-        this.setState({
-            email: '',
-            password: '',
-        });
+        const {email, password} = this.state;
+        try{
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({
+                email:'',
+                password:''
+            });
+        }
+        catch (e) {
+            console.error(e.message)
+        }
+
     };
     handleChange = (event) => {
         const {value, name} = event.target;
