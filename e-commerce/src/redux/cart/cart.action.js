2d4f7fa20860835d37b2a68a391e7016dbd8ab7a
Thai Duc Huy <huy.thai@ymail.com>
2020-06-16 17:14:54 +0700

    updated shop data updated cart util

--- e-commerce/src/redux/cart/cart.action.js
+++ e-commerce/src/redux/cart/cart.action.js
@@ -3,3 +3,7 @@ import {CartActionTypes} from './cart.types';
 export const toggleCart = () => ({
     type: CartActionTypes.TOGGLE_CART,
 });
+export const addItem = (item) => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+});
